42420a0b73178c3d3bf631f66bd11e3183596383
Adam Brandt <57259688+adkadt@users.noreply.github.com>
2026-08-11 22:48:13 -0400

    pigeon: added mode, events, and errors to system

--- pigeon/firmware/include/system.hpp
+++ pigeon/firmware/include/system.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
 namespace System {
+    void begin();
+    void update();
+
+    // Modes
+    enum class Mode {
+        FLIGHT,
+        GROUND
+    };
+
+    // States
     enum class State {
+        OFF,
         STARTUP,
         GPS_SEARCHING,
         GPS_LOCK,
@@ -9,19 +20,59 @@ namespace System {
         ERROR
     };
 
-    enum class Mode {
-        FLIGHT,
-        GROUND
+    // Errors
+    enum class Error {
+        NONE,
+
+        // GPS
+        GPS_DISCONNECTED,
+        GPS_TIMEOUT,
+
+        // LoRa
+        LORA_DISCONNECTED,
+        LORA_RECEIVE_FAILED,
+        LORA_TRANSMIT_FAILED,
+        LORA_PACKET_TOO_LARGE
     };
 
-    void begin();
-    void update();
+    // Events
+    enum class Event : uint32_t {
+        NONE = 0,
 
-    State getState();
+        // LoRa
+        LORA_TRANSMITTING,
+        LORA_TRANSMITTED,
+        LORA_RECEIVED,
+        LORA_PACKET_INVALID,
+        LORA_PACKET_TOO_LARGE,
+
+        // GPS
+        GPS_LOCKED,
+        GPS_LOST,
+
+        // Battery
+        BATTERY_LOW,
+        BATTERY_CRITICAL
+    };
+
+    // Modes
     Mode getMode();
-
-    void setState(State state);
-
-    bool stateChanged();
     bool modeChanged();
+    Mode getPreviousMode();
+
+    // States
+    State getState();
+    void setState(State state);
+    bool stateChanged();
+    State getPreviousState();
+
+    // Errors
+    Error getError();
+    void setError(Error error);
+    bool errorChanged();
+    Error getPreviousError();
+
+    // Events
+    void setEvent(Event event);
+    bool hasEvent(Event event);
 }
--- pigeon/firmware/src/system.cpp
+++ pigeon/firmware/src/system.cpp
@@ -1,56 +1,136 @@
 #include "system.hpp"
+#include <cstdint>
 
 namespace {
-    System::State lastState = System::State::STARTUP;
-    System::State currentState = System::State::STARTUP;
-    System::State nextState = System::State::STARTUP;
+    System::Mode readModeSwitch();
+    uint32_t eventMask(System::Event event);
 
     System::Mode lastMode = System::Mode::FLIGHT;
     System::Mode currentMode = System::Mode::FLIGHT;
+    
+    System::State lastState = System::State::OFF;
+    System::State currentState = System::State::STARTUP;
+    System::State nextState = System::State::STARTUP;
+    
+    System::Error lastError = System::Error::NONE;
+    System::Error currentError = System::Error::NONE;
+    System::Error nextError = System::Error::NONE;
 
-    System::Mode readModeSwitch();
+    uint32_t currentEvents = 0;
+    uint32_t nextEvents = 0;
 }
 
 void System::begin() {
-    lastState = State::STARTUP;
+    // Mode
+    lastMode = readModeSwitch();
+    currentMode = lastMode;
+
+    // State
+    lastState = State::OFF;
     currentState = State::STARTUP;
     nextState = State::STARTUP;
 
-    lastMode = readModeSwitch();
-    currentMode = lastMode;
+    // Error
+    lastError = System::Error::NONE;
+    currentError = System::Error::NONE;
+    nextError = System::Error::NONE;
 }
 
+
 void System::update() {
+    lastMode = currentMode;
+    currentMode = readModeSwitch();
+
     lastState = currentState;
     currentState = nextState;
     
-    lastMode = currentMode;
-    currentMode = readModeSwitch();
+    lastError = currentError;
+    currentError = nextError;
+
+    currentEvents = nextEvents;
+    nextEvents = 0;
 }
 
-System::State System::getState() {
-    return currentState;
-}
 
+// Mode Functions
 System::Mode System::getMode() {
     return currentMode;
-}
-
-void System::setState(System::State state) {
-    nextState = state;
-}
-
-bool System::stateChanged() {
-    return lastState != currentState;
 }
 
 bool System::modeChanged() {
     return lastMode != currentMode;
 }
 
+System::Mode System::getPreviousMode() {
+    return lastMode;
+}
+
+
+// State Functions
+System::State System::getState() {
+    return currentState;
+}
+
+void System::setState(System::State state) {
+    nextState = state;
+
+    if (nextState != System::State::ERROR)
+        System::setError(System::Error::NONE);
+}
+
+bool System::stateChanged() {
+    return lastState != currentState;
+}
+
+System::State System::getPreviousState() {
+    return lastState;
+}
+
+
+// Error Functions 
+System::Error System::getError() {
+    return currentError;
+}
+
+void System::setError(System::Error error) {
+    nextError = error;
+
+    if (nextError != System::Error::NONE)
+        System::setState(System::State::ERROR);
+}
+
+bool System::errorChanged() {
+    return lastError != currentError;
+}
+
+System::Error System::getPreviousError() {
+    return lastError;
+}
+
+
+// Event Functions
+
+
+void System::setEvent(System::Event event) {
+    nextEvents |= eventMask(event);
+}
+
+bool System::hasEvent(System::Event event) {
+    return (currentEvents & eventMask(event)) != 0;
+}
+
+
+// private implementation
 namespace {
     System::Mode readModeSwitch() {
         // eventually add mode switch reading.
         return System::Mode::FLIGHT;
+    }
+
+    uint32_t eventMask(System::Event event) {
+        if (event == System::Event::NONE)
+            return 0;
+    
+        return 1u << (static_cast<uint8_t>(event) - 1);
     }
 }
